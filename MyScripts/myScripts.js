41bef1b312311ea0b696a51103ad370870704478
tijojose27 <tijojose27@yahoo.com>
2017-10-26 17:38:33 -0500

    modal
    Created modals on image clicks

--- MyScripts/myScripts.js
+++ MyScripts/myScripts.js
@@ -1,7 +1,15 @@
 ﻿$(document).ready(function () {
+    //DESCRIPTION OF THE WEBSITES MUST BE PLACED HERE
+    //AND IN ORDER
+    var sites = [
+      "Layover made for a sales website : https://codepen.io/tijojose27/pen/yYPEEZ",
+      "Weather app with location from browser and weather api : https://codepen.io/tijojose27/pen/eGEeyy",
+      "Quotes app with random quotes from api : https://codepen.io/tijojose27/pen/KXvNLz",
+      "Top News app with New York Times api : https://codepen.io/tijojose27/pen/yzpvqG"
 
+    ]
     var scrollLink = $('.scroll');
-    
+
     scrollLink.click(function (e) {
         e.preventDefault();
         $('html,body').animate({
@@ -9,7 +17,32 @@
         }, 1000);
     });
 
-    $(window).scroll(function () {
-        var loc = $(this).scrollTop();
-    });
+    $('img').click(function(e){
+      e.preventDefault();
+      var alt = $(this).attr('alt');
+      var src = $(this).attr('src');
+      $(".modal-title").html(alt);
+      $("#modal-img").attr('src', src);
+      var index = $("img").index(this);
+      --index;
+      var info = "";
+      switch (index) {
+        case 0:
+          info = sites[index]
+          break;
+        case 1:
+          info = sites[index]
+          break;
+        case 2:
+          info = sites[index]
+          break;
+        case 3:
+          info = sites[index]
+          break;
+        default:
+          info="";
+      }
+      $("#modal-info").html(info);
+      $("#imgModal").modal();
+    })
 });
