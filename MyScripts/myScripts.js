d6790df8bbed32d2525a184ed522cda2f71d4e22
tijojose27 <tijojose27@yahoo.com>
2017-10-26 12:47:02 -0500

    Smooth scrolling
    Added smooth scrolling

--- MyScripts/myScripts.js
+++ MyScripts/myScripts.js
@@ -1,4 +1,18 @@
 ﻿$(document).ready(function () {
-    $('.scrollspy').scrollspy();
-});
+    $('body').scrollspy({
+        target: "#navScrollspy"
+    });
 
+    var scrollLink = $('.scroll');
+    
+    scrollLink.click(function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop:$(this.hash).offset().top
+        }, 1000);
+    });
+
+    $(window).scroll(function () {
+        var loc = $(this).scrollTop();
+    });
+});
